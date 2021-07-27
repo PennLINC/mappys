27d11f38fca115ccc9fb1b38fbe7dd90a9aaaf22
Maxwell Bertolero <mbertolero@me.com>
2021-07-27 10:01:33 -0400

    cleaner

--- make.py
+++ make.py
@@ -3,7 +3,6 @@ import os
 import numpy as np
 import pandas as pd
 
-# n_nodes = sys.argv[1]
 n_nodes = 400
 
 parcel_path = 'https://github.com/ThomasYeoLab/CBIG/raw/master/stable_projects/brain_parcellation/Schaefer2018_LocalGlobal/Parcellations/HCP/fslr32k/cifti/Schaefer2018_{0}Parcels_17Networks_order.dlabel.nii'.format(n_nodes)
@@ -48,66 +47,55 @@ df['CorrThickness'] = nib.load(out).get_fdata()[0]
 # evo
 lfile = 'lh.macaque-human.RelativeAreaExpansion.human.32k_fs_LR.nii.gz'
 rfile = 'rh.macaque-human.RelativeAreaExpansion.human.32k_fs_LR.nii.gz'
-if os.path.exists(lfile) == False:
-    os.system('wget https://github.com/TingsterX/alignment_macaque-human/raw/main/area_expansion/lh.macaque-human.RelativeAreaExpansion.human.32k_fs_LR.nii.gz')
-if os.path.exists(rfile) == False:
-    os.system('wget https://github.com/TingsterX/alignment_macaque-human/raw/main/area_expansion/rh.macaque-human.RelativeAreaExpansion.human.32k_fs_LR.nii.gz')
+if os.path.exists(lfile) == False: os.system('wget https://github.com/TingsterX/alignment_macaque-human/raw/main/area_expansion/lh.macaque-human.RelativeAreaExpansion.human.32k_fs_LR.nii.gz')
+if os.path.exists(rfile) == False: os.system('wget https://github.com/TingsterX/alignment_macaque-human/raw/main/area_expansion/rh.macaque-human.RelativeAreaExpansion.human.32k_fs_LR.nii.gz')
 df['evo_expansion'] = np.zeros((400))
 evo = np.zeros((p.shape[1]))
 evo[:32492] = nib.load(lfile).get_fdata().flatten()
 evo[32492:] = nib.load(rfile).get_fdata().flatten()
-
 for i in range(n_nodes):
     df['evo_expansion'][i] = evo[p[0]==i+1].mean()
-
 out = "evo_expansion_{0}.pscalar.nii".format(n_nodes)
 nib.save(nib.Cifti2Image(df['evo_expansion'].values.reshape(1,n_nodes),header=nib.load("corrthick_{0}.pscalar.nii".format(n_nodes)).header),out)
 
 
 # allo
-os.system('wget https://github.com/PennLINC/Brain_Organization/raw/master/AllometricScaling/rh.AllometricScaling_fsaverage5.func.gii')
-os.system('wget https://github.com/PennLINC/Brain_Organization/raw/master/AllometricScaling/lh.AllometricScaling_fsaverage5.func.gii')
-cmd = 'python freesurf2hcp.py lh.AllometricScaling_fsaverage5.func.gii rh.AllometricScaling_fsaverage5.func.gii alloscale'
-os.system(cmd)
-
-
+lfile = 'lh.AllometricScaling_fsaverage5.func.gii'
+rfile = 'rh.AllometricScaling_fsaverage5.func.gii'
+if os.path.exists(lfile) == False: os.system('wget https://github.com/PennLINC/Brain_Organization/raw/master/AllometricScaling/rh.AllometricScaling_fsaverage5.func.gii')
+if os.path.exists(lfile) == False: os.system('wget https://github.com/PennLINC/Brain_Organization/raw/master/AllometricScaling/lh.AllometricScaling_fsaverage5.func.gii')
 lfile = 'alloscale.32k_fs_LR.lh.shape.gii'
 rfile = 'alloscale.32k_fs_LR.rh.shape.gii'
+if os.path.exists(lfile) == False or os.path.exists(lfile) == False:
+    cmd = 'python freesurf2hcp.py lh.AllometricScaling_fsaverage5.func.gii rh.AllometricScaling_fsaverage5.func.gii alloscale' #convert freesufer to fslr32k
+    os.system(cmd)
 df['alloscale'] = np.zeros((400))
 evo = np.zeros((p.shape[1]))
 evo[:32492] = nib.load(lfile).agg_data().flatten()
 evo[32492:] = nib.load(rfile).agg_data().flatten()
-
 for i in range(n_nodes):
     df['alloscale'][i] = evo[p[0]==i+1].mean()
-
 out = "alloscale_{0}.pscalar.nii".format(n_nodes)
 nib.save(nib.Cifti2Image(df['alloscale'].values.reshape(1,n_nodes),header=nib.load("corrthick_{0}.pscalar.nii".format(n_nodes)).header),out)
 
 # cbf
-os.system('wget https://github.com/PennLINC/Brain_Organization/raw/master/MeanCBF/lh.MeanCBF.fsaverage5.func.gii')
-os.system('wget https://github.com/PennLINC/Brain_Organization/raw/master/MeanCBF/rh.MeanCBF.fsaverage5.func.gii')
-
 lfile = 'lh.MeanCBF.fsaverage5.func.gii'
 rfile = 'rh.MeanCBF.fsaverage5.func.gii'
-
-cmd = 'python freesurf2hcp.py lh.MeanCBF.fsaverage5.func.gii rh.MeanCBF.fsaverage5.func.gii cbf'
-os.system(cmd)
-
+if os.path.exists(lfile) == False: os.system('wget https://github.com/PennLINC/Brain_Organization/raw/master/MeanCBF/lh.MeanCBF.fsaverage5.func.gii')
+if os.path.exists(rfile) == False: os.system('wget https://github.com/PennLINC/Brain_Organization/raw/master/MeanCBF/rh.MeanCBF.fsaverage5.func.gii')
 lfile = 'cbf.32k_fs_LR.lh.shape.gii'
 rfile = 'cbf.32k_fs_LR.rh.shape.gii'
-
+if os.path.exists(lfile) == False or os.path.exists(lfile) == False:
+    cmd = 'python freesurf2hcp.py lh.MeanCBF.fsaverage5.func.gii rh.MeanCBF.fsaverage5.func.gii cbf' #convert freesufer to fslr32k
+    os.system(cmd)
 df['cbf'] = np.zeros((400))
 evo = np.zeros((p.shape[1]))
 evo[:32492] = nib.load(lfile).agg_data().flatten()
 evo[32492:] = nib.load(rfile).agg_data().flatten()
-
 for i in range(n_nodes):
     df['cbf'][i] = evo[p[0]==i+1].mean()
-
 out = "cbf_{0}.pscalar.nii".format(n_nodes)
 nib.save(nib.Cifti2Image(df['cbf'].values.reshape(1,n_nodes),header=nib.load("corrthick_{0}.pscalar.nii".format(n_nodes)).header),out)
 
-
-
+#save final dataframe
 df.to_csv('mappings_{0}.csv'.format(n_nodes))
